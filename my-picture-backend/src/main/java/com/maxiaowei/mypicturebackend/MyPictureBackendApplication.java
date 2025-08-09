d7adee55646971a75ab824b12659d2708f43c73d
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-08-10 01:22:26 +0800

    2025/08/10 2. 整合依赖 - MyBatis Plus 数据库操作

--- my-picture-backend/src/main/java/com/maxiaowei/mypicturebackend/MyPictureBackendApplication.java
+++ my-picture-backend/src/main/java/com/maxiaowei/mypicturebackend/MyPictureBackendApplication.java
@@ -1,9 +1,11 @@
 package com.maxiaowei.mypicturebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.maxiaowei.mypicturebackend.mapper")
 public class MyPictureBackendApplication {
 
     public static void main(String[] args) {
