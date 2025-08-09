21104ef5a1d7669e4081c912baf4042996ac104d
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-08-10 01:29:51 +0800

    2025/08/10 2. 整合依赖 - Knife4j 接口文档

--- my-picture-backend/src/main/java/com/maxiaowei/mypicturebackend/controller/PathVariableController.java
+++ my-picture-backend/src/main/java/com/maxiaowei/mypicturebackend/controller/PathVariableController.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.maxiaowei.mypicturebackend.demos.web;
+package com.maxiaowei.mypicturebackend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
--- my-picture-backend/src/main/java/com/maxiaowei/mypicturebackend/controller/User.java
+++ my-picture-backend/src/main/java/com/maxiaowei/mypicturebackend/controller/User.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.maxiaowei.mypicturebackend.demos.web;
+package com.maxiaowei.mypicturebackend.controller;
 
 /**
  * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
