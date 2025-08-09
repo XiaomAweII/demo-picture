58a0a66c534f5dcbf50c883feb12ff603925cc49
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-08-10 01:31:27 +0800

    2025/08/10 2. 整合依赖 - AOP 切面编程

--- my-picture-backend/src/main/java/com/maxiaowei/mypicturebackend/MyPictureBackendApplication.java
+++ my-picture-backend/src/main/java/com/maxiaowei/mypicturebackend/MyPictureBackendApplication.java
@@ -3,9 +3,11 @@ package com.maxiaowei.mypicturebackend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("com.maxiaowei.mypicturebackend.mapper")
+// @EnableAspectJAutoProxy(exposeProxy = true) // 通过 Spring AOP 提供对当前代理对象的访问，使得可以在业务逻辑中访问到当前的代理对象
 public class MyPictureBackendApplication {
 
     public static void main(String[] args) {
