558b73a7824a794133bb850bf6ab6e255482c739
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-08-10 17:16:39 +0800

    2025/08/10 用户模块 - 前端精度丢失

---
+++ my-picture-backend/src/main/java/com/maxiaowei/mypicturebackend/config/JsonConfig.java
@@ -0,0 +1,28 @@
+package com.maxiaowei.mypicturebackend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * Spring MVC Json 配置
+ * <p>
+ * 将整个后端 Spring MVC 接口返回值的长整型数字转换为字符串进行返回
+ * <p>
+ * 作者: maxiaowei
+ */
+@JsonComponent
+public class JsonConfig {
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        return objectMapper;
+    }
+}
